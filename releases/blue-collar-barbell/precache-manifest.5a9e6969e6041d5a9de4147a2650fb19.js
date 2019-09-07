1370f532b069644e0ccac5bac2224a474aba5e77
Sean Stangl <sean.stangl@gmail.com>
2019-09-07 14:50:45 -0600

    Add the "blue-collar-barbell" custom version
    Source is in the "custom-2019-09-07-blue-collar-barbell" branch of OpenLifter

---
+++ releases/blue-collar-barbell/precache-manifest.5a9e6969e6041d5a9de4147a2650fb19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64110c3135bd599cd71b",
+    "url": "/releases/blue-collar-barbell/static/js/runtime~main.64110c31.js"
+  },
+  {
+    "revision": "6efc73a3c7997a584992",
+    "url": "/releases/blue-collar-barbell/static/js/main.6efc73a3.chunk.js"
+  },
+  {
+    "revision": "82f8acb370d23c5ac150",
+    "url": "/releases/blue-collar-barbell/static/js/1.82f8acb3.chunk.js"
+  },
+  {
+    "revision": "6efc73a3c7997a584992",
+    "url": "/releases/blue-collar-barbell/static/css/main.1c9bcd3a.chunk.css"
+  },
+  {
+    "revision": "82f8acb370d23c5ac150",
+    "url": "/releases/blue-collar-barbell/static/css/1.c3d2b0b7.chunk.css"
+  },
+  {
+    "revision": "856776aee918b60b88ea58aebb625a4a",
+    "url": "/releases/blue-collar-barbell/index.html"
+  }
+];
