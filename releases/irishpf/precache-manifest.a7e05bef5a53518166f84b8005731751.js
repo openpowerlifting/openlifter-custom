8a85c3ea8305758ea5b38ce8c2732a5410a3d020
Sean Stangl <sean.stangl@gmail.com>
2019-11-10 16:31:35 -0700

    Add a custom version for the IrishPF, not finished yet.

---
+++ releases/irishpf/precache-manifest.a7e05bef5a53518166f84b8005731751.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65413858eaf811ac681c",
+    "url": "/releases/irishpf/static/css/main.36bfd100.chunk.css"
+  },
+  {
+    "revision": "65413858eaf811ac681c",
+    "url": "/releases/irishpf/static/js/main.65413858.chunk.js"
+  },
+  {
+    "revision": "d2e9a07c711da6f7a360",
+    "url": "/releases/irishpf/static/css/1.5aa96e5f.chunk.css"
+  },
+  {
+    "revision": "d2e9a07c711da6f7a360",
+    "url": "/releases/irishpf/static/js/1.d2e9a07c.chunk.js"
+  },
+  {
+    "revision": "9983f3fdf34e1c249336",
+    "url": "/releases/irishpf/static/js/runtime~main.9983f3fd.js"
+  },
+  {
+    "revision": "e2d1f735005b12b00d56c9ea396c57dd",
+    "url": "/releases/irishpf/index.html"
+  }
+];
