db0f669672f8ccdc78c840fa7a02eb0bfca63be7
Sean Stangl <sean.stangl@gmail.com>
2020-10-18 20:19:50 -0600

    Add the "gpc-nz/" custom version from Jared Klopper

---
+++ releases/gpc-nz/precache-manifest.c941f7483c5a98a3dc7af52066322361.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "85492f19c37746784eec",
+    "url": "/releases/gpc-nz/static/css/main.6a3b16f0.chunk.css"
+  },
+  {
+    "revision": "85492f19c37746784eec",
+    "url": "/releases/gpc-nz/static/js/main.85492f19.chunk.js"
+  },
+  {
+    "revision": "a4028997e1a3838995d0",
+    "url": "/releases/gpc-nz/static/css/1.8c95b44e.chunk.css"
+  },
+  {
+    "revision": "a4028997e1a3838995d0",
+    "url": "/releases/gpc-nz/static/js/1.a4028997.chunk.js"
+  },
+  {
+    "revision": "fce88dbdc0d32d102004",
+    "url": "/releases/gpc-nz/static/js/runtime~main.fce88dbd.js"
+  },
+  {
+    "revision": "43d6ff3601b04b7d75d3a3698c2161dd",
+    "url": "/releases/gpc-nz/static/media/gpc-nz-horizontal.43d6ff36.png"
+  },
+  {
+    "revision": "a0a4f7608dd3afbe2b3cbec1c947860d",
+    "url": "/releases/gpc-nz/static/media/gpc-nz-square.a0a4f760.png"
+  },
+  {
+    "revision": "d0c789df6da9c495ca5ec9c578ff97fa",
+    "url": "/releases/gpc-nz/index.html"
+  }
+];
