31148d7aecefdcdb9b905c60596049eaa15cf756
Sean Stangl <sean.stangl@gmail.com>
2020-12-05 08:46:56 -0700

    Add the burley-strength custom version

---
+++ releases/burley-strength/precache-manifest.1832dd19d2f4c0877b242e218058d085.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b77a62a2ead5993dea3",
+    "url": "/releases/burley-strength/static/css/main.6a3b16f0.chunk.css"
+  },
+  {
+    "revision": "8b77a62a2ead5993dea3",
+    "url": "/releases/burley-strength/static/js/main.8b77a62a.chunk.js"
+  },
+  {
+    "revision": "c77b47f41aa7919ed2f6",
+    "url": "/releases/burley-strength/static/css/1.8c95b44e.chunk.css"
+  },
+  {
+    "revision": "c77b47f41aa7919ed2f6",
+    "url": "/releases/burley-strength/static/js/1.c77b47f4.chunk.js"
+  },
+  {
+    "revision": "385e97fc7d64899ab96b",
+    "url": "/releases/burley-strength/static/js/runtime~main.385e97fc.js"
+  },
+  {
+    "revision": "16fb8024396375000ee33cbcb495b665",
+    "url": "/releases/burley-strength/static/media/burley-horizontal.16fb8024.png"
+  },
+  {
+    "revision": "0c8b40b5ef8dd72253d1671600fbb771",
+    "url": "/releases/burley-strength/static/media/gpc-aus-square.0c8b40b5.png"
+  },
+  {
+    "revision": "717a988dff31e5d111139a9ea676ee60",
+    "url": "/releases/burley-strength/index.html"
+  }
+];
