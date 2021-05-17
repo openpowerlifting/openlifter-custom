0e2a1455728e546258a8715757b97c95f38f3b53
Sean Stangl <sean.stangl@gmail.com>
2021-05-16 22:26:33 -0600

    Update to GPC-NZ 2020-05-16

---
+++ releases/gpc-nz/precache-manifest.d9d9dec6e24f5e8d6ebfc3a02ad2abfd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b69b2e9df64e6fabaab",
+    "url": "/releases/gpc-nz/static/css/main.5ca6631a.chunk.css"
+  },
+  {
+    "revision": "2b69b2e9df64e6fabaab",
+    "url": "/releases/gpc-nz/static/js/main.2b69b2e9.chunk.js"
+  },
+  {
+    "revision": "4425dafa182145ebaadc",
+    "url": "/releases/gpc-nz/static/css/1.e2683797.chunk.css"
+  },
+  {
+    "revision": "4425dafa182145ebaadc",
+    "url": "/releases/gpc-nz/static/js/1.4425dafa.chunk.js"
+  },
+  {
+    "revision": "fce88dbdc0d32d102004",
+    "url": "/releases/gpc-nz/static/js/runtime~main.fce88dbd.js"
+  },
+  {
+    "revision": "43d6ff3601b04b7d75d3a3698c2161dd",
+    "url": "/releases/gpc-nz/static/media/gpc-nz-horizontal.43d6ff36.png"
+  },
+  {
+    "revision": "a0a4f7608dd3afbe2b3cbec1c947860d",
+    "url": "/releases/gpc-nz/static/media/gpc-nz-square.a0a4f760.png"
+  },
+  {
+    "revision": "6bea1f5885f4d7c91cabc9b882409594",
+    "url": "/releases/gpc-nz/index.html"
+  }
+];
--- releases/gpc-nz/service-worker.js
+++ releases/gpc-nz/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/releases/gpc-nz/precache-manifest.6f5ba1b2352e512561f387f016c03fbd.js"
+  "/releases/gpc-nz/precache-manifest.d9d9dec6e24f5e8d6ebfc3a02ad2abfd.js"
 );
 
 workbox.clientsClaim();
